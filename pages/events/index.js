50018092b3b3b97a74ff7f1dbbd5ecf0098f6a6a
xnslx <xnslx89@gmail.com>
2021-04-06 23:10:14 -0500

    filter events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,21 @@
 import {getAllEvents} from '../../dummy-data';
 import EventList from '../../components/events/event-list';
-
+import EventsSearch from '../../components/events/events-search';
+import {Fragment} from 'react';
+import {useRouter} from 'next/router';
 
 function AllEventsPage() {
     const events = getAllEvents()
+    const router = useRouter()
+    const findEventsHandler = (year, month) => {
+        const fullpath = `/events/${year}/${month}`
+        router.push(fullpath)
+    }
     return(
-        <div>
+        <Fragment>
+            <EventsSearch onSearch={findEventsHandler}/>
             <EventList items={events}/>
-        </div>
+        </Fragment>
     )
 };
 
