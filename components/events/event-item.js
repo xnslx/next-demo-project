a3705a46f3d06b501c7a695743491f767426eccf
xnslx <xnslx89@gmail.com>
2021-04-13 23:04:27 -0500

    optimization

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import Link from 'next/link';
 import classes from './event-item.module.css';
 import Button from '../ui/button';
@@ -20,7 +22,7 @@ function EventItem(props) {
     console.log('explorelink', exploreLink)
     return(
         <li className={classes.item}>
-            <img src={'/' + image} alt={title}/>
+            <Image src={'/' + image} alt={title} width={250} height={160}/>
             <div className={classes.content}>
                 <div className={classes.summary}>
                     <h2>{title}</h2>
--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,14 @@ import Layout from '../components/layout/layout';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-    return 
+    return (
         <Layout>
             <Head>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
             </Head>
             <Component {...pageProps }/>
         </Layout>
+    )
 }
 
 export default MyApp
