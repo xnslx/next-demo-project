0910c70fd19431c3fd90d614c7edeb7d4a95003c
xnslx <xnslx89@gmail.com>
2021-04-18 22:10:20 -0500

    add newsletter api routes

--- pages/events/index.js
+++ pages/events/index.js
@@ -16,9 +16,9 @@ function AllEventsPage(props) {
     return(
         <Fragment>
             <Head>
-        <title>NextJS Events</title>
-        <meta name="description" content="Find a lot of great events that allow you to evolve."/>
-      </Head>
+                <title>NextJS Events</title>
+                <meta name="description" content="Find a lot of great events that allow you to evolve."/>
+            </Head>
             <EventsSearch onSearch={findEventsHandler}/>
             <EventList items={events}/>
         </Fragment>
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import {getFeaturedEvents} from '../helpers/api-util';
 import EventList from '../components/events/event-list';
+import NewsletterRegistration from '../components/input/newsletter-registration';
 
 function Home(props) {
 
@@ -11,6 +12,7 @@ function Home(props) {
         <title>NextJS Events</title>
         <meta name="description" content="Find a lot of great events that allow you to evolve."/>
       </Head>
+      <NewsletterRegistration />
       <EventList items={props.events}/>
     </div>
   )
