85e8c505ef4531479fd6e3b0bebe1b3fe1261263
xnslx <xnslx89@gmail.com>
2021-04-18 23:18:39 -0500

    connect with db

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,12 +1,22 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+require('dotenv').config();
+
+const dbUrl = process.env.URL;
+
+async function handler(req, res) {
     if (req.method === 'POST') {
         const userEmail = req.body.email;
         if (!userEmail || !userEmail.includes('@')) {
             res.status(422).json({ message: 'Invalid email address.' })
             return;
         }
+        const client = await MongoClient.connect(dbUrl)
+        const db = client.db();
 
-        console.log(userEmail)
+        await db.collection('emails').insertOne({ email: userEmail });
+
+        client.close();
+
         res.status(201).json({ message: 'Signed up.' })
     }
 };
