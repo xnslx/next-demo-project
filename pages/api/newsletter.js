f53ba3c6c3aa6d29124f79f40079aedda8372ca4
xnslx <xnslx89@gmail.com>
2021-04-19 22:09:15 -0500

    error handling

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -3,6 +3,20 @@ require('dotenv').config();
 
 const dbUrl = process.env.URL;
 
+async function connectDatabase() {
+    const client = await MongoClient.connect(dbUrl)
+
+    return client;
+}
+
+async function insertDocument(client, document) {
+    const db = client.db();
+
+    const result = await db.collection('emails').insertOne(document);
+
+    return result;
+}
+
 async function handler(req, res) {
     if (req.method === 'POST') {
         const userEmail = req.body.email;
@@ -10,12 +24,20 @@ async function handler(req, res) {
             res.status(422).json({ message: 'Invalid email address.' })
             return;
         }
-        const client = await MongoClient.connect(dbUrl)
-        const db = client.db();
-
-        await db.collection('emails').insertOne({ email: userEmail });
-
-        client.close();
+        let client;
+        try {
+            client = await connectDatabase();
+        } catch (error) {
+            res.status(500).json({ message: 'Connecting to the database failed.' });
+            return;
+        }
+        try {
+            await insertDocument(client, { email: userEmail });
+            client.close();
+        } catch (error) {
+            res.status(500).json({ message: 'Inserting data failed.' });
+            return;
+        }
 
         res.status(201).json({ message: 'Signed up.' })
     }
