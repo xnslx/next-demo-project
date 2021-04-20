5f46dc26b7a6631d96d376772ffda2ba300f35af
xnslx <xnslx89@gmail.com>
2021-04-19 22:51:03 -0500

    usecontext

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Layout from '../components/layout/layout';
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Notification from '../components/ui/notification';
 
 function MyApp({ Component, pageProps }) {
     return (
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }) {
                 <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
             </Head>
             <Component {...pageProps }/>
+            <Notification title="Test" message="This is a test" status="pending"/>
         </Layout>
     )
 }
