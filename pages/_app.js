57d492a5e3044e330fa9c8ebcc2ccbb7cd04dd3d
xnslx <xnslx89@gmail.com>
2021-04-19 22:59:49 -0500

    create context store

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,19 @@ import Head from 'next/head';
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
 import Notification from '../components/ui/notification';
+import {NotificationContextProvider} from '../store/notification-context';
 
 function MyApp({ Component, pageProps }) {
     return (
-        <Layout>
-            <Head>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-            </Head>
-            <Component {...pageProps }/>
-            <Notification title="Test" message="This is a test" status="pending"/>
-        </Layout>
+        <NotificationContextProvider>
+            <Layout>
+                <Head>
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+                </Head>
+                <Component {...pageProps }/>
+                <Notification title="Test" message="This is a test" status="pending"/>
+            </Layout>
+        </NotificationContextProvider>
     )
 }
 
