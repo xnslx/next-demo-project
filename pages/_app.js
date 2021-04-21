1ae3e0197f0bb5adabdd9af682d048fa5abbb106
xnslx <xnslx89@gmail.com>
2021-04-20 21:54:40 -0500

    show notification

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import Head from 'next/head';
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
-import Notification from '../components/ui/notification';
 import {NotificationContextProvider} from '../store/notification-context';
 
 function MyApp({ Component, pageProps }) {
+
+
     return (
         <NotificationContextProvider>
             <Layout>
@@ -12,7 +13,6 @@ function MyApp({ Component, pageProps }) {
                     <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
                 </Head>
                 <Component {...pageProps }/>
-                <Notification title="Test" message="This is a test" status="pending"/>
             </Layout>
         </NotificationContextProvider>
     )
